104bb1042c93dd690138d4e9e6de9b7311ff5444
allen.han <18237133763@163.com>
2014-07-18 08:34:16 +0800

    ssh

--- ssh-core/src/main/java/allen/han/ssh/core/entity/User.java
+++ ssh-core/src/main/java/allen/han/ssh/core/entity/User.java
@@ -1,5 +1,61 @@
 package allen.han.ssh.core.entity;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
 
+@Entity //声明实体bean
+@Table(name="t_user")
+public class User {
+	private int id;
+	private String username;
+	private String nickname;
+	private String email;
+	private String password;
+	private String sex;
+	
+	@Id //声明Bean的标识属性
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Transient
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	
 }
